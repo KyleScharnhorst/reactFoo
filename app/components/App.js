42f15855a1e712f42ab24b03cb286cf513c3fd59
KyleScharnhorst <scharkr1@uw.edu>
2017-08-17 03:42:25 -0700

    Change: Replace tabs with spaces.

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export class App extends React.Component {
-	render () {
-		return (
-			<div>
-				<h1>hello world!</h1>
-			</div>
-		);
-	}
+  render () {
+    return (
+      <div>
+        <h1>hello world!</h1>
+      </div>
+    );
+  }
 }
